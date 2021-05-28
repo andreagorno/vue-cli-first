6d9d21f71dc5379ab83f6948f6039f612fa6b90b
andreagorno <andreagorno98@gmail.com>
2021-05-28 14:36:45 +0200

    aggiunto Title

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+    font-size: 60px;
+    text-align: center;
+    color: #42b983;
+}
+</style>
